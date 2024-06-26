4434583e3464000d91e2bab6486f44ce7c038981
Rohith-yadav07 <rohithnaniyadav1122@gmail.com>
2024-06-26 13:36:43 +0530

    popup

---
+++ pop.js
@@ -0,0 +1,12 @@
+function display() {
+
+    document.getElementById("box") .style.display = "block";
+}
+function cancel () {
+
+    document.getElementById("box") .style.display = "none";
+}
+function del() {
+
+    document.getElementById("button") .style.display = "none";
+}
